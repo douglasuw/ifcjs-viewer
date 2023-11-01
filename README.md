dc04fa3095ea8afe13a4417f7ea9e76da202d14a
douglasuw <douglasuw@gmail.com>
2023-11-01 12:05:40 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Viewer IFC with IFCjs
+
+#### Visualizador de modelos 3D para engenheiros e arquitetos
+
+Acesse em: https://douglasuw.github.io/ifcjs-viewer
---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+	input: 'app.js',
+	output: {
+		format: 'esm',
+		file: 'bundle.js'
+	},
+	plugins: [resolve()]
+};
